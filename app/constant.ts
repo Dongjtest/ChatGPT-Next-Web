8b44c6b51c789e9b37759ab98af03fcff5457199
Dongjtest <134114388+Dongjtest@users.noreply.github.com>
2025-01-13 11:31:52 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -624,5 +624,5 @@ export const internalAllowedWebDavEndpoints = [
 
 export const DEFAULT_GA_ID = "G-89WN60ZK2E";
 
-export const SAAS_CHAT_URL = "https://nextchat.dev/chat";
-export const SAAS_CHAT_UTM_URL = "https://nextchat.dev/chat?utm=github";
+export const SAAS_CHAT_URL = "https://ai.xm.gs";
+export const SAAS_CHAT_UTM_URL = "ai.xm.gs";
